5c871af8dfc1fff28cd81ec8605598565981952b
kennedgi <kennedgi@oregonstate.edu>
2025-11-12 05:58:19 +0800

    Prototype

--- mandelbrot.cs
+++ mandelbrot.cs
@@ -1,6 +1,8 @@
 using Godot;
 using System;
 using System.Threading;
+using System.Diagnostics;
+using Silk.NET.OpenCL;
 
 [GlobalClass]
 public partial class mandelbrot : RefCounted
@@ -10,43 +12,248 @@ public partial class mandelbrot : RefCounted
 	
 	private static byte[] data;
 	private static int threadCount;
+	private static int workGroupCount;
 	
-	private static float offsetX = 0.75f;
-	private static float zoomLevel = 4.0f;
+	private static float zoomLevel = 1.0f;
 	
-	public static void ChangeTextureSize(int newWidth, int newHeight)
-	{
-		width = newWidth;
-		height = newHeight;
-		data = new byte[newWidth * newHeight * 3];
+	private static int mandelbrotType = 0;
+	
+	private static bool isRendering = false;
+	private static int threadsCompleted = 0;
+	private static int lastType = 0;
+	
+	private static Stopwatch sw = new Stopwatch();
+	private static float lastTime = 0f;
+	
+	private static bool isFinished = false;
+	
+	public static bool IsFinished() {
+		return isFinished;
 	}
 	
-	public static void ChangeThreadCount(int newThreadCount) {
+	public static float GetTime() {
+		return lastTime;
+	}
+	
+	public static int GetLastThreadCount() {
+		return threadCount;
+	}
+	
+	public static int GetLastMandelbrotType() {
+		return mandelbrotType;
+	}
+	
+	public static int GetLastWorkGroupCount() {
+		return workGroupCount;
+	}
+	
+	public static int GetMaxWorkGroupSize()
+	{
+		unsafe
+		{
+			var cl = CL.GetApi();
+			
+			uint platformCount = 0;
+			cl.GetPlatformIDs(0, null, &platformCount);
+			
+			Span<nint> platforms = new nint[(int)platformCount];
+			fixed (nint* pPlatforms = platforms)
+			{
+				cl.GetPlatformIDs(platformCount, pPlatforms, null);
+				
+				uint deviceCount = 0;
+				cl.GetDeviceIDs(platforms[0], DeviceType.All, 0, null, &deviceCount);
+				
+				Span<nint> devices = new nint[(int)deviceCount];
+				fixed (nint* pDevices = devices)
+				{
+					cl.GetDeviceIDs(platforms[0], DeviceType.All, deviceCount, pDevices, null);
+					
+					ulong workGroupSize = 0;
+					cl.GetDeviceInfo(devices[0], DeviceInfo.MaxWorkGroupSize, sizeof(ulong), &workGroupSize, null);
+					return (int)workGroupSize;
+				}
+			}
+		}
+	}
+	
+	public static void SetNewZoom(int newZoom) {
+		zoomLevel = newZoom;
+	}
+
+	public static void StartMandelbrotRender(int newWidth, int newHeight, int newThreadCount, int newMandelbrotType, int newWorkGroupCount)
+	{
+		if (isRendering)
+			return;
+		
+		isFinished = false;
+		isRendering = true;
+		threadsCompleted = 0;
+		lastTime = 0f;
+		width = newWidth;
+		height = newHeight;
 		threadCount = newThreadCount;
+		workGroupCount = newWorkGroupCount;
+		mandelbrotType = newMandelbrotType;
+		data = new byte[newWidth * newHeight * 3];
+		
+		sw.Start();
+		
+		if (mandelbrotType == 0) {
+			Mandelbrot();
+		}
+		else if (mandelbrotType == 1) {
+			MandelbrotGPU();
+		}
 	}
 		
 	public static byte[] GetData() {
 		return data;
 	}
 	
-	public static void MandelbrotTiled()
+	public static void Mandelbrot()
 	{
-		System.Array.Clear(data, 0, data.Length);
-		
 		int tileWidth = width / threadCount;
 		
 		for (int t = 0; t < threadCount; t++)
 		{
-			int startX = t * tileWidth;
-			int endX = (t == threadCount - 1) ? width : startX + tileWidth;
+			int index = t;
+			int startX = index * tileWidth;
+			int endX = (index == threadCount - 1) ? width : startX + tileWidth;
 			
-			Thread thread = new Thread(() => RenderMandelbrot(startX, endX));
-			thread.IsBackground = true;
+			Thread thread = null;
+			
+			thread = new Thread(() => RenderMandelbrot(index, threadCount));
+				
+			thread.IsBackground = false;
 			thread.Start();
 		}
 	}
+	
+	public static void MandelbrotGPU()
+	{
+		if (!isGPUInitialized)
+		{
+			InitializeGPU();
+		}
+		
+		unsafe
+		{
+			int w = width;
+			int h = height;
+			float z = zoomLevel;
+			nint buf = gpuBuffer;
+			
+			cl.SetKernelArg(gpuKernel, 0, (nuint)sizeof(nint), &buf);
+			cl.SetKernelArg(gpuKernel, 1, (nuint)sizeof(int), &w);
+			cl.SetKernelArg(gpuKernel, 2, (nuint)sizeof(int), &h);
+			cl.SetKernelArg(gpuKernel, 3, (nuint)sizeof(float), &z);
+			
+			nuint globalWorkSize = (nuint)(width * height);
+			nuint localWorkSize = (nuint)workGroupCount;
+			
+			cl.EnqueueNdrangeKernel(gpuQueue, gpuKernel, 1, null, &globalWorkSize, &localWorkSize, 0, null, null);
+			cl.Finish(gpuQueue);
+			
+			fixed (byte* pData = data)
+			{
+				cl.EnqueueReadBuffer(gpuQueue, gpuBuffer, true, 0, (nuint)(width * height * 3), pData, 0, null, null);
+			}
+			
+			FinishMandelbrot();
+		}
+	}
 
-	private static void RenderMandelbrot(int startX, int endX)
+	private static bool isGPUInitialized = false;
+	private static nint gpuContext;
+	private static nint gpuQueue;
+	private static nint gpuProgram;
+	private static nint gpuKernel;
+	private static nint gpuBuffer;
+	private static nint gpuDevice;
+	private static CL cl;
+
+	private static void InitializeGPU()
+{
+	unsafe
+	{
+		cl = CL.GetApi();
+		
+		nint platform;
+		nint device;
+		cl.GetPlatformIDs(1, &platform, null);
+		cl.GetDeviceIDs(platform, DeviceType.Gpu, 1, &device, null);
+		gpuDevice = device;
+
+		int err;
+		nint dev = gpuDevice;
+		gpuContext = cl.CreateContext(null, 1, &dev, null, null, &err);
+		gpuQueue = cl.CreateCommandQueue(gpuContext, gpuDevice, CommandQueueProperties.None, &err);
+		
+		string kernelSource = @"
+__kernel void mandelbrot(__global uchar* output, int width, int height, float zoom) {
+	int index = get_global_id(0);
+	if (index >= width * height) return;
+	
+	int px = index % width;
+	int py = index / width;
+	
+	float scale = width * zoom;
+	float x0 = -0.746f + (px - width * 0.5f) / scale;
+	float y0 = 0.09f + (py - height * 0.5f) / scale;
+	
+	float x = 0.0f, y = 0.0f;
+	int i;
+	for (i = 1; i < 10000; i++) {
+		float mag = x*x + y*y;
+		if (mag > 4.0f) break;
+		float xtemp = x*x - y*y + x0;
+		y = 2.0f*x*y + y0;
+		x = xtemp;
+	}
+	
+	int outIndex = index * 3;
+if (i < 10000) {
+	float t = (i / 255.0f);
+	
+	output[outIndex] = (uchar)(t * 30);
+	output[outIndex + 1] = (uchar)(t * 80);
+	output[outIndex + 2] = (uchar)(150 + t * 105);
+} else {
+	output[outIndex] = 0;
+	output[outIndex + 1] = 0;
+	output[outIndex + 2] = 0;
+}
+}";
+		
+		byte[] sourceBytes = System.Text.Encoding.ASCII.GetBytes(kernelSource);
+		nuint sourceLength = (nuint)sourceBytes.Length;
+		
+		nint prog;
+		nint kern;
+		nint buf;
+		
+		prog = cl.CreateProgramWithSource(gpuContext, 1, new string[] { kernelSource }, null, &err);
+
+		cl.BuildProgram(prog, 1, &dev, "", null, null);
+
+		byte[] kernelName = System.Text.Encoding.ASCII.GetBytes("mandelbrot\0");
+		fixed (byte* pKernelName = kernelName)
+		{
+			kern = cl.CreateKernel(prog, pKernelName, &err);
+		}
+
+		buf = cl.CreateBuffer(gpuContext, MemFlags.ReadWrite, (nuint)(width * height * 3), null, &err);
+		
+		gpuProgram = prog;
+		gpuKernel = kern;
+		gpuBuffer = buf;
+		
+		isGPUInitialized = true;
+	}
+}
+
+	private static void RenderMandelbrotTile(int startX, int endX)
 	{
 		for (int px = startX; px < endX; px++)
 		{
@@ -55,11 +262,48 @@ public partial class mandelbrot : RefCounted
 				ProcessPixel(px, py);
 			}
 		}
+		
+		ThreadCompleted();
 	}
+	
+	private static void RenderMandelbrot(int threadIndex, int threadCount)
+	{
+		for (int py = threadIndex; py < height; py += threadCount)
+		{
+			for (int px = 0; px < width; px++)
+			{
+				ProcessPixel(px, py);
+			}
+		}
+		
+		ThreadCompleted();
+	}
+	
+	private static void ThreadCompleted() {
+		threadsCompleted++;
+		if (threadsCompleted == threadCount)
+		{
+			FinishMandelbrot();
+		}
+	}
+	
+	private static void FinishMandelbrot() {
+		isRendering = false;
+		isFinished = true;
+		sw.Stop();
+		lastTime = sw.ElapsedMilliseconds;
+		sw.Reset();
+	}
+	
 	private static void ProcessPixel(int px, int py)
 	{
-		float x = (px - (offsetX * width)) / (width / zoomLevel);
-		float y = (py - (width / zoomLevel)) / (width / zoomLevel);
+		float scale = width * zoomLevel;
+		
+		float centerX = -0.746f;
+		float centerY = 0.09f;
+				
+		float x = centerX + (px - width * 0.5f) / scale;
+		float y = centerY + (py - height * 0.5f) / scale;
 		
 		Color color = CalculateMandelbrot(x, y);
 		
@@ -97,10 +341,12 @@ public partial class mandelbrot : RefCounted
 	
 	private static Color RgbConv(int i)
 	{
-		float hue = i / 255.0f;
-		float saturation = 1.0f;
-		float brightness = 0.75f;
+		float t = (i / 255.0f);
 		
-		return Color.FromHsv(hue, saturation, brightness);
+		return new Color(
+			t * 30 / 255.0f,
+			t * 80 / 255.0f,
+			(150 + t * 105) / 255.0f
+		);
 	}
 }
--- ui.gd
+++ ui.gd
@@ -1,6 +1,13 @@
 extends Node
 
 @export var texture_rect: Node
+@export var max_cpu_cores_text : Label
+@export var max_gpu_cores_text : Label
+@export var h_slider: HSlider
+@export var h_slider2: HSlider
+@export var cpu_on_text : Label
+@export var zoom_text : Label
+@export var results_text : Label
 
 var img: Image
 
@@ -8,17 +15,63 @@ var img_height = 512
 var img_width = 1024
 
 var max_thread_count
+var thread_count = 1
+
+var max_work_group_count
+var work_group_count = 1
+
+var cpu_on = true
+
+var current_zoom = 1.0
 
 func _ready():
-	mandelbrot.ChangeTextureSize(img_width, img_height)
 	max_thread_count = OS.get_processor_count()
-	mandelbrot.ChangeThreadCount(16)
+	max_work_group_count = mandelbrot.GetMaxWorkGroupSize()
+	h_slider.max_value = max_thread_count
+	h_slider2.max_value = max_work_group_count
+	cpu_slider_changed(1)
+	gpu_slider_changed(1)
 	img = Image.create(img_width, img_height, false, Image.FORMAT_RGB8)
-
+	
 func _process(_delta: float):
-	img.set_data(img_width, img_height, false, Image.FORMAT_RGB8, mandelbrot.GetData())
-	var texture = ImageTexture.create_from_image(img)
-	texture_rect.texture = texture
-		
+	if mandelbrot.IsFinished():
+		var data = mandelbrot.GetData()
+		img.set_data(img_width, img_height, false, Image.FORMAT_RGB8, data)
+		var texture = ImageTexture.create_from_image(img)
+		texture_rect.texture = texture
+		if mandelbrot.GetTime() != 0:
+			if mandelbrot.GetLastMandelbrotType() == 0:
+				results_text.text = "%d thread(s) took %dms" % [mandelbrot.GetLastThreadCount(), mandelbrot.GetTime()]
+			elif mandelbrot.GetLastMandelbrotType() == 1:
+				results_text.text = "%d work group(s) took %dms" % [mandelbrot.GetLastWorkGroupCount(), mandelbrot.GetTime()]
+	
 func do_mandelbrot():
-	mandelbrot.MandelbrotTiled()
+	if cpu_on:
+		mandelbrot.StartMandelbrotRender(img_width, img_height, thread_count, 0, work_group_count)
+	else:
+		mandelbrot.StartMandelbrotRender(img_width, img_height, thread_count, 1, work_group_count)
+	
+func cpu_slider_changed(value: float) -> void:
+	thread_count = value
+	max_cpu_cores_text.text = "CPU Thread Count: %d (Max: %d)" % [thread_count, max_thread_count]
+	
+func gpu_slider_changed(value: float) -> void:
+	work_group_count = value
+	max_gpu_cores_text.text = "GPU Work Group Count: %d (Max: %d)" % [work_group_count, max_work_group_count]
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	cpu_on = toggled_on
+	cpu_on_text.text = "CPU" if toggled_on else "GPU"
+	
+func zoom_in():
+	current_zoom = current_zoom * 2.0
+	_update_zoom()
+	
+func zoom_out():
+	if current_zoom > 1.0:
+		current_zoom = current_zoom / 2.0
+		_update_zoom()
+
+func _update_zoom():
+	zoom_text.text = "Zoom: %dx" % [current_zoom]
+	mandelbrot.SetNewZoom(current_zoom)
